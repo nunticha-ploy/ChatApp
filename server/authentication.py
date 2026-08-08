3906d047d0b44eb1d80e5cee93653fe1fbdccb3c
FeihongChuSecE <faye.chu.fh@gmail.com>
2026-08-07 21:37:12 -0400

    update user part
    login/ logout/ signup
    Need to improve: multiple-user chat in one terminal

---
+++ server/authentication.py
@@ -0,0 +1,63 @@
+import hashlib #The hashlib module is used to hash the password before saving it to the user details file.
+
+#hash the password
+user_details_file = "user.txt"
+
+def hash_password(password):
+  pwd_bytes = password.encode('utf-8')
+  hashed_pwd = hashlib.sha256(pwd_bytes).hexdigest()
+  return hashed_pwd
+
+#save the user details
+def save_user(username, email, hashed_pwd):
+  with open(user_details_file, "a") as f: #a to append new users.
+    f.write(f"{username} {email} {hashed_pwd}\n")
+
+
+#signup
+def signup(username, email, password):
+  #username
+  if not username.strip():
+    return False, "Username cannot be empty"
+  
+  #email
+  if user_exists(email):
+    return False, "This email is already registered."
+  if not email.strip():
+    return False, "Email cannot be empty."
+  
+  #password
+  if not password.strip():
+    return False, "Password cannot be empty."
+  
+  #save user
+  hashed_pwd = hash_password(password)
+  save_user(username, email, hashed_pwd)
+  return True, "You have registered successfully!"
+
+#check if the user exists
+def user_exists(email):
+  try:
+    with open(user_details_file, "r") as f:
+      for line in f:
+        parts = line.split()
+        if len(parts) >= 2 and parts[1] == email:
+          return True
+  except FileNotFoundError:
+    return False
+  return False
+
+
+#login
+def login(email, password):
+  auth_hash = hash_password(password)
+  #read the file line by line
+  try:                    
+    with open(user_details_file, "r") as f:
+      for line in f:
+        username, stored_email, stored_hash = line.split()
+        if email == stored_email and auth_hash == stored_hash:
+          return True, "Logged in Successfully!", username
+  except FileNotFoundError:
+    return False, "No registered users.", None
+  return False, "Login failed!", None
---
+++ server/server.py
@@ -0,0 +1,107 @@
+import socket
+import argparse
+import threading #This module lets us create multiple threads
+from authentication import login, signup
+
+host = "localhost"
+data_buff = 2048 #receive up to 2048 bytes at a time
+backlog = 5 #up to 5 clients can wait to connect.
+port = 5000
+
+def userLoginHandle(client, address):
+  print("Clent connected:", address)
+
+  try: 
+    data = client.recv(data_buff) 
+    #Check if data exists 
+    if data:
+      message = data.decode().strip()
+      #removes any accidental spaces before or after the message and makes the parsing a little more robust
+      parts = message.strip().split()
+      if not parts:
+        client.sendall("Cannot be empty" .encode())
+        return
+
+      #login
+      if parts[0] == "LOGIN":
+        if len(parts) != 3:
+          client.sendall("Invalid LOGIN." .encode())
+        else:
+          #client send data 
+          email = parts[1]
+          password = parts[2]
+          success, reply, username = login(email, password)
+          if success:
+            client.sendall(f"SUCCESS {username}" .encode())
+          else:
+            client.sendall(reply.encode())
+
+          #keeps the loggin client connected
+          if success:
+            while True:
+              data = client.recv(data_buff) #Wait for the next message from the client.
+              if not data:
+                break
+              message = data.decode().strip()
+              #logout
+              if message.upper() == "LOGOUT":
+                client.sendall("Logout successfully!" .encode())
+                break
+              print(f"{username}: {message}")
+              client.sendall(("Server received: " + message).encode())
+      #singup
+      elif parts[0] == "SIGNUP":
+        if len(parts) != 4:
+          client.sendall("Invalid SINGUP." .encode())
+        else:
+          username = parts[1]
+          email = parts[2]
+          password = parts[3]
+          success, reply = signup(username, email, password)
+          client.sendall(reply.encode())
+      #other choose
+      else:
+        message = data.decode().strip()
+        print("Client:", message)
+
+        response = "Server received: " + message
+        client.send(response.encode())
+  except Exception as error:
+    print("Error:", error)
+  finally:
+    client.close()
+    print("Client disconnected:", address) 
+
+#create a TCP socket
+def TCPserver1(port):
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+  #bind it to a port
+  server_address = (host, port)
+  print("Starting up echo server on %s port %s" % server_address)
+  sock.bind(server_address)
+
+  #listen       
+  sock.listen(backlog)
+
+  #loop
+  while True:
+    print("Waiting to receive message from client")
+    
+    #accept clients
+    client, address = sock.accept() ## accept new connection
+
+    #start a threading
+    thread = threading.Thread(target=userLoginHandle, args=(client, address))
+    thread.start()
+
+   
+
+#Main program
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="Socket Server Example")
+  parser.add_argument("--port", action="store", dest="port", type=int, required=True)
+  given_args = parser.parse_args()
+  port = given_args.port
+  TCPserver1(port)  
